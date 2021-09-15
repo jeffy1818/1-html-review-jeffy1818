733ffe0b007440235279dc5c0813398fe0b4879c
jeffy1818 <jeffrey.yu1818@gmail.com>
2021-09-15 09:52:14 -0400

    Class VueJs
    Class Notes to be converted to HW#3

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -5,5 +5,20 @@ const Counter = {
       }
     }
   }
+
+  created() {
+    fetch('https://randomuser.me/api')
+    .then(
+      function(response) {
+        return response.json()
+      }
+    )
+    .then(
+      function(json) {
+        console.log(json);
+        this.result = json.results[0];
+      }
+    )
+  }
   
   Vue.createApp(Counter).mount('#counter')
