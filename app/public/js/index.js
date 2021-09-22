037680e6e83d4161e3944e4c2e4f7a5a9f9bb4d6
jeffy1818 <jeffrey.yu1818@gmail.com>
2021-09-21 22:23:34 -0400

    Creating Random User
    Using VueJs to link Random User API

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,24 +1,34 @@
-const Counter = {
-    data() {
-      return {
-        counter: 0
-      }
+const RandomUser = {
+  data() {
+    return {
+      result: {},
     }
+  },
+  computed: {
+      CustomDOB() {
+          /*Pulled from RandomUser, but now formatted */ 
+          return dayjs(this.result.dob.date)
+          /*Custom Formatting*/ 
+          .format('MMM D, YYYY')
+      }
+  },
+  methods: {
+      fetchUserData() {
+          fetch('https://randomuser.me/api/')
+          .then(response => response.json())
+          .then( (json) => {
+              console.log("A");
+              this.result = json.results[0];
+              console.log("C");
+          })
+          .catch( (error) => {
+              console.error(error);
+          });
+      }
+  },
+  created() {
+      this.fetchUserData();
   }
 
-  created() {
-    fetch('https://randomuser.me/api')
-    .then(
-      function(response) {
-        return response.json()
-      }
-    )
-    .then(
-      function(json) {
-        console.log(json);
-        this.result = json.results[0];
-      }
-    )
-  }
-  
-  Vue.createApp(Counter).mount('#counter')
+}
+Vue.createApp(RandomUser).mount('#RandomUser');
