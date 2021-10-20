664cc9647c27e5cc4d7945bd5f739e4e7ff2c052
jeffy1818 <jeffrey.yu1818@gmail.com>
2021-10-20 13:23:53 -0400

    HW6

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,16 @@ FROM php:7.4-apache
 
 LABEL maintainer="Jeffrey Yu"
 
+RUN docker-php-ext-install pdo_mysql
+
+#Copy our app folder to the image
 COPY app /srv/app
 
+# PHP configuration
+COPY docker/php/php.ini /usr/local/etc/php/php.ini
 
+# Apache configuration
 COPY docker/apache/vhost.conf /etc/apache2/sites-available/000-default.conf
+
+#Set the working directory in the image
+WORKDIR /srv/app
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,8 @@
 const SomeApp = {
     data() {
       return {
-        students: [],
-        selectedStudent: null,
-        offers: [],
-        offerForm: {},
-        books: []
+        books: [],
+        bookForm: {}
       }
     },
     computed: {},
@@ -18,37 +15,6 @@ const SomeApp = {
             const d = new Intl.NumberFormat("en-US").format(n);
             return "$ " + d;
         },
-        selectStudent(s) {
-            if (s == this.selectedStudent) {
-                return;
-            }
-            this.selectedStudent = s;
-            this.offers = [];
-            this.fetchOfferData(this.selectedStudent);
-        },
-        fetchStudentData() {
-            fetch('/api/student/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.students = responseJson;
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-        },
-        fetchOfferData(s) {
-            console.log("Fetching offer data for ", s);
-            fetch('/api/offer/?student=' + s.id)
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.offers = responseJson;
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-        },
         fetchBooksData() {
             fetch('/api/books/')
             .then( response => response.json() )
@@ -59,10 +25,27 @@ const SomeApp = {
             .catch( (err) => {
                 console.error(err);
             });
-        }
+        },
+        postNewBook(evt) {
+            console.log("Posting!", this.bookForm);
+            fetch('api/books/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                // reset the form
+                this.bookForm = {};
+              });
+        },
     },
     created() {
-        this.fetchStudentData();
         this.fetchBooksData();
     }
   
